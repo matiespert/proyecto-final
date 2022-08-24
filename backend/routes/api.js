c51651fd05bf9a97cfcf5cf1b3f81f5841513ac2
matiespert <matias.espert.laciar@gmail.com>
2022-08-24 14:43:56 -0300

    aca subo el formulario funcionando

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -2,6 +2,8 @@ var express = require('express');
 var router = express.Router();
 var novedadesModel = require('./../models/novedadesModel');
 var cloudinary = require('cloudinary').v2;
+var nodemailer = require('nodemailer')
+
 
 router.get('/novedades', async function (req,res,next){
     let novedades = await novedadesModel.getNovedades();
@@ -25,6 +27,31 @@ router.get('/novedades', async function (req,res,next){
         }
     });
     res.json(novedades);
+});
+
+
+router.post('/contacto', async (req, res)=> {
+    const mail = {
+        to:"matias.espert.laciar@gmail.com",
+        subjetc: 'Contacto Web',
+        html: `${req.body.nombre} se contacto a traves de la web y quiere mas información a este correo: ${req.body.email} <br> Además, hizo el siguiente comentario: ${req.body.mensaje}<br> Su tel es: ${req.body.telefono}`
+    }
+
+    const trasnport = nodemailer.createTransport({
+        host: process.env.SMTP_HOST,
+        port: process.env.SMTP_PORT,
+        auth: {
+            user: process.env.SMTP_USER,
+            pass: process.env.SMTP_PASS
+        }
+    });
+
+    await trasnport.sendMail(mail)
+
+    res.status(201).json({
+        error: false,
+        message: "Mensaje enviado"
+    });
 });
 
 module.exports = router;
--- frontend/src/pages/ContactoPage.js
+++ frontend/src/pages/ContactoPage.js
@@ -1,35 +1,77 @@
 import '../styles/ContactPage.css';
+import { useState } from 'react';
+import axios from 'axios';
 
 
 
 
 const ContactoPage = (props) => {
+
+
+    const initialForm = {
+        nombre: '',
+        email: '',
+        telefono: '',
+        mensaje: ''
+    }
+
+    const [sending, setSending] = useState(false);
+    const [msg, setMsg] = useState(false);
+    const [formData, setFormData] = useState(initialForm);
+
+    const handleChange = e => {
+        const { name, value } = e.target;
+        setFormData(oldData => ({
+            ...oldData,
+            [name]: value
+        }));
+    }
+
+    const handleSubmit = async e => {
+        e.preventDefault();
+        setMsg('');
+        setSending(true)
+        const response = await axios.post('http://localhost:3000/api/contacto', formData);
+        setSending(false);
+        setMsg(response.data.message);
+        if (response.data.error === false) {
+            setFormData(initialForm)
+        }
+    }
+
+
+
+
+
+
     return (
         <div>
             <main class="holder contacto">
 
+               
+
                 <div class="form">
                     <h2 style={{ color: '#fff', fontWeight: '100', marginLeft: '20px', fontSize: '30px' }}>Contactanos!</h2>
-                    
-                    <form action="" method="" class="formulario">
+
+                    <form action="/contacto" method="post" className="formulario" onSubmit={handleSubmit}>
 
                         <div class="input-group">
-                            <input required="" type="text" name="text" autocomplete="off" class="input" />
+                            <input required="" type="text" autocomplete="off" class="input" name="nombre" value={formData.nombre} onChange={handleChange} />
                             <label class="user-label">Nombre</label>
                         </div>
 
                         <div class="input-group">
-                            <input required="" type="text" name="text" autocomplete="off" class="input" />
+                            <input required="" type="text" autocomplete="off" class="input" name="email" value={formData.email} onChange={handleChange} />
                             <label class="user-label" for="email">Email</label>
                         </div>
 
                         <div class="input-group">
-                            <input required="" type="text" name="text" autocomplete="off" class="input" />
+                            <input required="" type="text" autocomplete="off" class="input" name="telefono" value={formData.telefono} onChange={handleChange} />
                             <label class="user-label">Teléfono</label>
                         </div>
 
                         <div class="input-group">
-                            <input required="" type="text" name="text" autocomplete="off" class="input mensaje" />
+                            <input required="" type="text" autocomplete="off" class="input mensaje" name="mensaje" value={formData.mensaje} onChange={handleChange} />
                             <label class="user-label">Mensaje</label>
                         </div>
 
@@ -46,10 +88,14 @@ const ContactoPage = (props) => {
 
 
                     </form>
+
+                    {sending ? <p style={{color:'#fff', marginLeft: '50px'}}>Enviando 🚀​​</p> : null}
+                {msg ? <p style={{color:'#fff', marginLeft: '50px'}}>{msg}✅​</p> : null}
+
                 </div>
 
                 <div class="datos">
-                    <h2 style={{color: '#fff', fontSize:'30px', fontWeight:'100'}}>Otras vias de comunicacion</h2>
+                    <h2 style={{ color: '#fff', fontSize: '30px', fontWeight: '100' }}>Otras vias de comunicacion</h2>
 
 
                     <ul class="lista-contacto">
